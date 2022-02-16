15a387fc25d29e2c10ea57b526ee11d4ab608893
Okrio <jingbiao_huang@163.com>
2022-02-17 00:17:22 +0800

    add dataset.py

---
+++ dataset/dataset.py
@@ -0,0 +1,8 @@
+'''
+Author: Okrio
+Date: 2022-02-17 00:14:37
+LastEditTime: 2022-02-17 00:17:10
+LastEditors: Please set LastEditors
+Description: training dataset generating and validation
+FilePath: /CRUSE/dataset/dataset.py
+'''
